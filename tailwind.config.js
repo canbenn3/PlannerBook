7e58a8945925b823a0beff74ba3eab35c1265c18
Bennett Cannon <canbenn3@gmail.com>
2024-09-23 10:28:53 -0600

    Restarting

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,ts,js,jsx,tsx,md,mdx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-}
-
