120635e11c6f65eea8791ee99abae7bcb011e716
Bennett Cannon <canbenn3@gmail.com>
2024-12-14 09:08:18 -0700

    updated goals page

--- goals.js
+++ goals.js
@@ -1,10 +1,20 @@
 const addBtns = document.getElementsByClassName("add");
+const mask = document.getElementById("mask");
+const editBtns = document.getElementsByClassName("edit");
+let current;
 
 for (let btn of addBtns) {
   btn.addEventListener("click", () => {
     const section = btn.parentElement;
     const category = section.parentElement.id;
-    console.log(category);
-    document.getElementById(category).dataset.open = true;
+    const type = category.split("-")[0];
+    current = type;
+    document.getElementById(`${type}-modal`).dataset.open = true;
+    mask.dataset.open = "true";
   });
 }
+
+mask.addEventListener("click", () => {
+  mask.dataset.open = "false";
+  document.getElementById(`${current}-modal`).dataset.open = "false";
+});
